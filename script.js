01c6d9aa40d6f1a8b9eca30c2a00e35bda3cd92b
draunzler <shadymeister2017@gmail.com>
2024-12-24 13:24:15 +0530

    removed the hamburger menu and changed the color of learn more page

--- script.js
+++ script.js
@@ -110,14 +110,14 @@
 //   }
 // });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
-  const mobileNav = document.querySelector(".mobile-nav");
+// document.addEventListener("DOMContentLoaded", function() {
+//   const hamburgerMenu = document.querySelector(".hamburger-menu");
+//   const mobileNav = document.querySelector(".mobile-nav");
 
-  hamburgerMenu.addEventListener("click", function() {
-      mobileNav.classList.toggle("active");
-  });
-});
+//   hamburgerMenu.addEventListener("click", function() {
+//       mobileNav.classList.toggle("active");
+//   });
+// });
 
 document.addEventListener('DOMContentLoaded', () => {
   const domainName = window.location.hostname;
